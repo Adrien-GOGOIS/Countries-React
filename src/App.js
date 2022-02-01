5290dd8bee2b1a928d95ec7975eebb8485d9443d
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-01 14:46:43 +0100

    Add functionnal country selection buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
+// COMPONENTS
+import Button from "./components/Button";
+
 class App extends React.Component {
   constructor() {
     super();
@@ -11,11 +14,14 @@ class App extends React.Component {
       flag: "",
       population: "",
       region: "",
+      selectedCountry: "",
     };
+
+    this.getCountry = this.getCountry.bind(this);
   }
 
   componentDidMount() {
-    fetch("https://restcountries.com/v3.1/name/france")
+    fetch(`https://restcountries.com/v3.1/name/france`)
       .then((res) => res.json())
       .then((res) => {
         this.setState({
@@ -28,13 +34,28 @@ class App extends React.Component {
       });
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevState, this.state);
+  getCountry(country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          name: res[0].name.common,
+          capital: res[0].capital,
+          flag: res[0].flag,
+          population: res[0].population,
+          region: res[0].region,
+        });
+      });
   }
 
   render() {
     return (
       <div>
+        <div>
+          <Button onClick={() => this.getCountry("france")}>France</Button>
+          <Button onClick={() => this.getCountry("brazil")}>Brazil</Button>
+          <Button onClick={() => this.getCountry("croatia")}>Croatia</Button>
+        </div>
         <p>{this.state.name}</p>
         <p>{this.state.capital}</p>
         <p>{this.state.flag}</p>
