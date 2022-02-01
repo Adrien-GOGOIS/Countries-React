214c850fea431025dafd33e77e44dc0a6692fdfa
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-01 17:25:35 +0100

    Add some style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class Button extends React.Component {
   render() {
-    return <button onClick={this.props.onClick}>{this.props.children}</button>;
+    return (
+      <button className="btn btn-primary m-2" onClick={this.props.onClick}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,14 @@ import React from "react";
 class Card extends React.Component {
   render() {
     return (
-      <div>
-        <p>{this.props.displayFlag}</p>
-        <p>Country : {this.props.countryName}</p>
-        <p>Capital : {this.props.capitalName}</p>
-        <p>Continent : {this.props.regionName}</p>
-        <p>Population : {this.props.populationNum} habitants</p>
+      <div className="card">
+        <p className="fs-6">{this.props.displayFlag}</p>
+        <div className="card-body">
+          <p className="card-title">Country : {this.props.countryName}</p>
+          <p>Capital : {this.props.capitalName}</p>
+          <p>Continent : {this.props.regionName}</p>
+          <p>Population : {this.props.populationNum} habitants</p>
+        </div>
       </div>
     );
   }
